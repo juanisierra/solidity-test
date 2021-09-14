d13a62eff26666755519b8bc20d7feae9bd23cda
Juan Ignacio Sierra <juanignaciosierra96@gmail.com>
2021-09-14 22:24:45 +0200

    Added contract tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,31 +2,36 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-// Mocha test example
+const INITIAL_STRING = 'Hi there!';
+let accounts;
+let inbox;
 
-class Car {
-  park() {
-    return 'stopped';
-  }
+beforeEach(async () => {
 
-  drive() {
-    return 'vroom';
-  }
-}
+  //get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-let car;
-
-beforeEach(() => {
-  car = new Car();
+  //use account to deploy contracts
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING]})
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
-describe('Car', () => {
-  it('Can park', () => {
-    assert.equal(car.park(), 'stopped');
-  })
+describe('deploys a contract', () => {
+  it('Inbox contract', () => {
+    assert.ok(inbox.options.address); // the Deployed contract actually has an address as it's been correctly deployed
+  });
 
-  it('Can drive', () => {
-    assert.equal(car.drive(), 'vroom');
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call(); //We just want to call the message method, calls are free
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('new message').send({ from: accounts[0] }); // As we have an action, we need to send a Tx and specify who pays, it returns a Tx hash
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'new message');
   })
 });
